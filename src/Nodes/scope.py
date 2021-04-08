a7abbe4d4ed44f8b38c3349808aef60a7df0c3f2
Andrew Sanchez <andrewfsanchez@gmail.com>
2021-04-07 22:04:50 -0400

    Makes top level scope's have enclosingFunction as self

--- src/Nodes/scope.py
+++ src/Nodes/scope.py
@@ -1,8 +1,11 @@
 class Scope:
-    def _init_(self, parent=None, enclosingFunction=None):
+    def _init_(self, parent=None):
         self.parent = parent
-        self.enclosingFunction = enclosingFunction
-        vars = {}
+        self.enclosingFunction = self
+        if parent is not None:
+            self.enclosingFunction = self.parent.enclosingFunction
+        
+        self.vars = {}
 
     def update(self, variable, value):
         self.vars.update({variable: value})
